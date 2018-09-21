a5600645b29f7a8c50437792427a1ee84021aefe
Michael Moschitto <michaelmoschitto@gmail.com>
2018-09-21 11:58:22 -0700

    final commit, test passed

--- planets.py
+++ planets.py
@@ -1,7 +1,11 @@
 def weight_on_planets():
    # write your code here
-   
-   
-   
+   weightEarth = input("What do you weigh on earth? ") #take in user weight using test input
+   weightEarthFloat = float(weightEarth)
+   weightMars = weightEarthFloat * .38
+   weightJupiter = weightEarthFloat * 2.34
+
+   print("\nOn Mars you would weigh " + str(weightMars) + " pounds.\n" + "On Jupiter you would weigh " + str(weightJupiter) + " pounds.")
+
 if __name__ == '__main__':
    weight_on_planets()
